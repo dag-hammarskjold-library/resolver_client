1d1c6de91d7cafc042f2bbc5e8ad8b9e742fc2de
Andrew Lee <fiacre.patrick@gmail.com>
2017-12-11 14:36:47 -0500

    throw doc symbols and titles into a table -- for demo purposes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from logging import getLogger
 from sqlalchemy.exc import SQLAlchemyError
+import json
 from .models import DocumentMetadata
 from .db import db_session
 from .base_page import BasePage
@@ -31,12 +32,15 @@ def page_not_found(e):
 def index():
     if request.method == "POST":
         doc_symbols = request.form.get('document_symbols', None)
+        ctx = {}
         symbol_list = []
+        metadata_json = ''
         if doc_symbols:
             symbols = doc_symbols.split(',')
             symbol_list = [sym.strip() for sym in symbols]
             for elem in symbol_list:
                 metadata_q = db_session.query(DocumentMetadata).filter_by(document_symbol=elem)
+                # if not in DB, go fetch
                 if not metadata_q.count():
                     metadata_json = _fetch_metadata(elem)
                     dm = DocumentMetadata(document_symbol=elem, metadata_json=metadata_json)
@@ -44,9 +48,12 @@ def index():
                     db_session.add(dm)
                     db_session.commit()
                 else:
-                    pass
+                    query = db_session.query(DocumentMetadata).filter_by(document_symbol=elem).first()
+                    metadata_json = query.metadata_json
 
-            return render_template('results.html', context=symbol_list)
+                ctx[elem] = json.loads(metadata_json)
+
+            return render_template('results.html', context=ctx)
 
     else:
         return render_template('index.html')
